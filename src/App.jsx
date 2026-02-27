2b385dcca6c636e2100f11cd6dfb052698e1ec57
nitin2292 <nitin.22scse1012375@gagotiasuniversity.edu.in>
2026-02-28 02:07:44 +0530

    chnages made in teh display

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 
 import Header from './components/Header'
 import Todoitems from './components/Todoitems'
-
-
+import Button from './components/Button'
+import './App.css'
 
 function App() {
   
   return (
    <>
+   <div className='app-container'>
    <Header/>
    <Todoitems/>
+    <Todoitems/>
+     <Todoitems/>
+      <Todoitems/>
+       <Todoitems/>
+   <Button/>
+    </div>
    </>
   )
 }
